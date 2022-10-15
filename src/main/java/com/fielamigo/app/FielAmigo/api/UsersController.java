7a51d07e6e0bf85a87cb7fd41040d4fd1fd5228b
Radomir Vladislavić <vladislavic.rl@gmail.com>
2022-10-14 23:01:49 -0400

    changed response code

--- src/main/java/com/fielamigo/app/FielAmigo/api/UsersController.java
+++ src/main/java/com/fielamigo/app/FielAmigo/api/UsersController.java
@@ -1,10 +1,12 @@
 package com.fielamigo.app.FielAmigo.api;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fielamigo.app.FielAmigo.bl.UserBl;
@@ -22,6 +24,7 @@ public class UsersController {
     }
 
     @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
     public UserDto createUser(@RequestBody UserDto user) {
         System.out.println("Creating user...");
         return userBl.createUser(user);
